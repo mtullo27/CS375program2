f303158a9cc07d137609e81ad50373c921934fd6
Matt Tullo <mtullo1@binghamton.edu>
2020-04-15 17:44:17 -0400

    Commit

--- program2.cpp
+++ program2.cpp
@@ -1,8 +1,8 @@
-#include<stddef.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<fstream>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fstream>
 #include <iostream>
 #include <cmath>
 #include <vector> 
@@ -10,3 +10,17 @@
 
 using namespace std;
 
+int max(int a, int b) {
+	return (a > b) ? a : b;
+}
+int main(int argc, char* argv[]) {
+	ifstream infile;
+	ofstream outfile;
+
+	string file;
+
+	infile.open(argv[1]);
+	outfile.open(argv[2]);
+
+	infile >> file;
+ }
